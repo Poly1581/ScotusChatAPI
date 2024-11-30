c774af83ce6f5d2dab51ab4872ca17821dd6ce0c
Brandon Paulsen <bspaulsen@dons.usfca.edu>
2024-11-29 17:00:23 -0800

    Added comments

--- Backend/src/main/java/com/example/demo/BookController.java
+++ Backend/src/main/java/com/example/demo/BookController.java
@@ -30,74 +30,105 @@ import dev.langchain4j.model.openai.OpenAiChatModel;
 
 @RestController
 public class BookController {
-
+  /**
+   * Basic assistant interface
+   */
   private static interface Assistant {
     String chat(String userMessage);
   }
 
   private final String apiKey = Dotenv.configure().load().get("OPENAI_API_KEY");
 
-  private final BookRepository bookRepository;
-
   private final Map<String, Map<String, Assistant>> assistants = new HashMap<>();
 
-  public BookController(BookRepository bookRepository) {
-    this.bookRepository = bookRepository;
+  public BookController() {
     System.out.println(apiKey);
   }
 
+  /**
+   * Route to get years
+   * @return the list of years for which cases are supported
+   */
   @GetMapping("/getYears")
   @ResponseBody
   @CrossOrigin(origins = "*")
   public List<Integer> getYears() {
-    File[] directories = new File("embeddings").listFiles(File::isDirectory);
+    //Get directories within embeddings directory
+    File[] embeddingsDirectory = new File("embeddings").listFiles(File::isDirectory);
     List<Integer> years = new ArrayList<>();
-    for(File directory : directories) {
-      years.add(Integer.parseInt(directory.getName()));
+    //Get names of all subdirectories
+    for(File yearDirectory : embeddingsDirectory) {
+      years.add(Integer.parseInt(yearDirectory.getName()));
     }
     return years;
   }
 
+  /**
+   * Route to get cases for a given year
+   * @param year the year whose cases are to be returned
+   * @return A list of case names for a given year
+   */
   @GetMapping("/getCases")
   @ResponseBody
   @CrossOrigin(origins = "*")
   public List<String> getCases(@RequestParam int year) {
-    System.out.println("embeddings/" + year);
-    File[] embeddings = new File("embeddings/" + year).listFiles();
+    //Get directory for given year
+    File[] yearDirectory = new File("embeddings/" + year).listFiles();
     List<String> caseNames = new ArrayList<>();
-    for(File embedding : embeddings) {
+    //Get all case names for the given year
+    for(File embedding : yearDirectory) {
       String name = embedding.getName();
-      caseNames.add(name.substring(0,name.indexOf(".store")));
+      caseNames.add(name.replace(".store",""));
     }
     return caseNames;
   }
 
+  /**
+   * Route to chat with a given case
+   * @param userName name of user chatting
+   * @param year year of the case
+   * @param caseName name of the case
+   * @param userMessage message to ask agent
+   * @return response of agent
+   */
   @GetMapping("/chat")
   @ResponseBody
   @CrossOrigin(origins = "*")
   public String chat(@RequestParam String userName, @RequestParam int year, @RequestParam String caseName, @RequestParam String userMessage) {
     try {
+      //Get assistants associated with the user
       Map<String, Assistant> userAssistants = assistants.get(userName);
       if (userAssistants == null) {
+        //Make empty hashmap of assistants and associate it with the user
         assistants.put(userName, new HashMap<>());
         userAssistants = assistants.get(userName);
       }
+      //Get assistant associated with the user and case
       Assistant caseAssistant = userAssistants.get(caseName);
       if (caseAssistant == null) {
+        //Load pre-processed embedding store for the case
         InMemoryEmbeddingStore<TextSegment> embedding = InMemoryEmbeddingStore.fromFile("embeddings/" + year + "/" + caseName + ".store");
+        //Create an openai chat model
         ChatLanguageModel chatModel = OpenAiChatModel.builder()
                 .apiKey(apiKey)
                 .modelName(OpenAiChatModelName.GPT_4_O_MINI)
                 .build();
+        //Create an assistant
         Assistant assistant = AiServices.builder(Assistant.class)
+                //Link the openai chat model
                 .chatLanguageModel(chatModel)
+                //Add chat memory
                 .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                //Add content retriever (rag with case information)
                 .contentRetriever(EmbeddingStoreContentRetriever.from(embedding))
+                //Add system message
                 .systemMessageProvider(chatMemoryId -> "You are a constitutional law professor explaining supreme court cases. Respond concisely, correctly, and accurately.")
                 .build();
+        //Associate assistant with user and case
         userAssistants.put(caseName, assistant);
         caseAssistant = userAssistants.get(caseName);
       }
+      //Prompt assistant with userMessage and return response
       return caseAssistant.chat(userMessage);
     } catch (Exception e) {
       System.out.println(e.getMessage());
